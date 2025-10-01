a15fbe85fc5049b1b324bbf4982c2e08addc5e31
ethanhope719-cell <ethanhope719@gmail.com>
2025-10-01 17:56:51 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -39,8 +39,9 @@ def store():
     
     
     resp = requests.post(url, json=payload)
+   
     if resp.status_code != 200:
-        print()
+        print(resp.text)
         
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
